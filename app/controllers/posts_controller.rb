1aa7a66bc8a4ea5bc7b66588fc31297754300198
fasfafal <nogulin0530@gmail.com>
2023-06-17 20:29:39 -0700

    ツイートの新規投稿の実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,43 @@
 class PostsController < ApplicationController
   def index
+    @posts = Post.all.order(created_at: :desc)
+  end
+  
+  def show
+    @post = Post.find_by(id: params[:id])
+  end
+  
+  def new
+    @post = Post.new
+  end
+  
+  def create
+    @post = Post.create(post_params)
+    if @post.save
+    flash[:notice] = "投稿を作成しました。"
+    redirect_to posts_path
+    else
+      render "new", status: :unprocessable_entity
+    end
+  end
+  
+  def edit
+    @post = Post.find_by(id: params[:id])
+  end
+
+  def update
+    @post = Post.find_by(id: params[:id])
+    @post.update(post_params)
+    redirect_to posts_path
+  end
+
+  def destroy
+    @post = Post.find_by(id: params[:id])
+    @post.destroy
+    redirect_to posts_path
+  end
+  private
+  def post_params
+    params.require(:post).permit(:content)
   end
 end
